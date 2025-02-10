5f793d0904aeed6a33bbb461ecd7cddfcb3b8d97
blackbird-47 <gswastik1996@gmail.com>
2025-02-10 14:44:51 +0530

    Used drawer navigation

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
 import {View, StyleSheet, useColorScheme, Image} from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Ionicons } from '@expo/vector-icons';
 import Menu from '../components/Menu';
 import Login from '../components/Login';
 import Homescreen from '../components/Homescreen';
 
 //const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
+//const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 const logo = () => {
     return <Image 
@@ -20,25 +22,11 @@ const logo = () => {
 
 export default function App() {
     return (
-            <Tab.Navigator initialRouteName='homescreen' screenOptions={({route}) => ({
-                tabBarIcon: ({focused, color, size}) => {
-                    let iconName;
-                    if (route.name === 'homescreen') {
-                        iconName = focused ? 'home' : 'home-outline';
-                    } else if (route.name === 'login') {
-                        iconName = focused ? 'log-in' : 'log-in-outline';
-                    } else if (route.name === 'menu') {
-                        iconName = focused ? 'menu' : 'menu-outline';
-                    }
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                },
-                tabBarActiveTintColor: '#dda15e',
-                tabBarInactiveTintColor: '#283618',
-                })}>
-                <Tab.Screen name="homescreen" component={Homescreen} options={{headerTitle: logo}}/>
-                <Tab.Screen name="login" component={Login} options={{title: "Login"}}/>
-                <Tab.Screen name="menu" component={Menu} options={{title: "Menu"}}/>
-            </Tab.Navigator>   
+            <Drawer.Navigator initialRouteName='homescreen'>
+                <Drawer.Screen name="homescreen" component={Homescreen} options={{headerTitle: logo, title: "Home"}}/>
+                <Drawer.Screen name="login" component={Login} options={{title: "Login"}}/>
+                <Drawer.Screen name="menu" component={Menu} options={{title: "Menu"}}/>
+            </Drawer.Navigator>   
     );
 }
 
